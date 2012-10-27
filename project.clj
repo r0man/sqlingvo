f70d1aff1a18876b3412089e275218d1d81fb0f2
Roman Scherer <roman.scherer@burningswell.com>
2012-10-27 22:56:15 +0200

    Added url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sqlingvo "0.1.0-SNAPSHOT"
   :description "A SQL DSL in Clojure."
-  :url "http://example.com/FIXME"
+  :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
