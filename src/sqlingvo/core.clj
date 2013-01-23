9dd357662b769705b01443f8229dd5c299e41b0f
Roman Scherer <roman.scherer@burningswell.com>
2013-01-23 22:25:26 +0100

    Use domonad and :when.

--- src/sqlingvo/core.clj
+++ src/sqlingvo/core.clj
@@ -28,18 +28,18 @@
 (defn cascade
   "Returns a fn that adds a CASCADE clause to an SQL statement."
   [cascade?]
-  (fn [stmt]
-    (if cascade?
-      [nil (assoc stmt :cascade {:op :cascade})]
-      [nil stmt])))
+  (domonad state-m
+    [:when cascade?
+     _ (set-val :cascade {:op :cascade})]
+    nil))
 
 (defn continue-identity
   "Returns a fn that adds a CONTINUE IDENTITY clause to an SQL statement."
   [continue-identity?]
-  (fn [stmt]
-    (if continue-identity?
-      [nil (assoc stmt :continue-identity {:op :continue-identity})]
-      [nil stmt])))
+  (domonad state-m
+    [:when continue-identity?
+     _ (set-val :continue-identity {:op :continue-identity})]
+    nil))
 
 (defn desc
   "Parse `expr` and return an ORDER BY expr using descending order."
@@ -121,25 +121,25 @@
 (defn if-exists
   "Returns a fn that adds a IF EXISTS clause to an SQL statement."
   [if-exists?]
-  (fn [stmt]
-    (if if-exists?
-      [nil (assoc stmt :if-exists {:op :if-exists})]
-      [nil stmt])))
+  (domonad state-m
+    [:when if-exists?
+     _ (set-val :if-exists {:op :if-exists})]
+    nil))
 
 (defn if-not-exists
   "Returns a fn that adds a IF EXISTS clause to an SQL statement."
   [if-not-exists?]
-  (fn [stmt]
-    (if if-not-exists?
-      [nil (assoc stmt :if-not-exists {:op :if-not-exists})]
-      [nil stmt])))
+  (domonad state-m
+    [:when if-not-exists?
+     _ (set-val :if-not-exists {:op :if-not-exists})]
+    nil))
 
 (defn inherits
   "Returns a fn that adds an INHERITS clause to an SQL statement."
   [& tables]
-  (let [inherits (map parse-table tables)]
-    (fn [stmt]
-      [nil (assoc stmt :inherits inherits)])))
+  (domonad state-m
+    [_ (set-val :inherits (map parse-table tables))]
+    nil))
 
 (defn insert
   "Returns a fn that builds a INSERT statement."
@@ -227,18 +227,18 @@
 (defn restart-identity
   "Returns a fn that adds a RESTART IDENTITY clause to an SQL statement."
   [restart-identity?]
-  (fn [stmt]
-    (if restart-identity?
-      [nil (assoc stmt :restart-identity {:op :restart-identity})]
-      [nil stmt])))
+  (domonad state-m
+    [:when restart-identity?
+     _ (set-val :restart-identity {:op :restart-identity})]
+    nil))
 
 (defn restrict
   "Returns a fn that adds a RESTRICT clause to an SQL statement."
   [restrict?]
-  (fn [stmt]
-    (if restrict?
-      [nil (assoc stmt :restrict {:op :restrict})]
-      [nil stmt])))
+  (domonad state-m
+    [:when restrict?
+     _ (set-val :restrict {:op :restrict})]
+    nil))
 
 (defn returning
   "Returns a fn that adds a RETURNING clause to an SQL statement."
@@ -265,10 +265,10 @@
 (defn temporary
   "Returns a fn that adds a TEMPORARY clause to an SQL statement."
   [temporary?]
-  (fn [stmt]
-    (if temporary?
-      [nil (assoc stmt :temporary {:op :temporary})]
-      [nil stmt])))
+  (domonad state-m
+    [:when temporary?
+     _ (set-val :temporary {:op :temporary})]
+    nil))
 
 (defn truncate
   "Returns a fn that builds a TRUNCATE statement."
