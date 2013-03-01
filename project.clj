e854d1b9e6c443918b726269f04af86721e33eb6
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:08:00 +0100

    Switched to Clojure 1.5.0.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [inflections "0.7.8"]]
+                 [inflections "0.8.0"]]
   :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.21"]
                                   [postgresql "9.1-901.jdbc4"]
                                   [org.xerial/sqlite-jdbc "3.7.2"]]}})
