ba132481514698a2f2de7af43415cbecfc4da2b4
Roman Scherer <roman.scherer@burningswell.com>
2013-03-27 20:41:30 +0100

    Switched to clojur 1.5.1.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [inflections "0.8.0"]]
