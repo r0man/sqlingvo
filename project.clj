dfb6e42369874d7b245145307202d93ac00ce3a4
Roman Scherer <roman.scherer@burningswell.com>
2013-04-12 16:55:13 +0200

    Reverting "Switched to org.clojure/java.jdbc 0.3.0-alpha1." Revert "Switched to org.clojure/java.jdbc 0.3.0-alpha1."
    This reverts commit 7774bf779d1d827cdea164153653f2f5cac9121c.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.monads "0.1.0"]
-                 [org.clojure/java.jdbc "0.3.0-alpha1"]
+                 [org.clojure/java.jdbc "0.2.3"]
                  [inflections "0.8.0"]]
   :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.21"]
                                   [postgresql "9.1-901.jdbc4"]
