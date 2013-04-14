2e5117fe083d7d5707409cc11f6846578645ba9f
Roman Scherer <roman.scherer@burningswell.com>
2013-04-14 11:19:26 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/algo.monads "0.1.0"]
+                 [org.clojure/algo.monads "0.1.4"]
                  [org.clojure/java.jdbc "0.3.0-alpha1"]
-                 [inflections "0.8.0"]]
+                 [inflections "0.8.1"]]
   :profiles {:dev {:dependencies [[mysql/mysql-connector-java "5.1.21"]
                                   [postgresql "9.1-901.jdbc4"]
                                   [org.xerial/sqlite-jdbc "3.7.2"]]}})
