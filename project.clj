2f7d6281983a5324168d036377de705f7624e8ae
Roman Scherer <roman.scherer@burningswell.com>
2013-04-17 22:38:13 +0200

    Started 0.4.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlingvo "0.4.0"
+(defproject sqlingvo "0.4.1-SNAPSHOT"
   :description "A SQL DSL in Clojure."
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
