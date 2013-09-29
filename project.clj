2ea4158f39eee565b4c13782cb4f90971ff44d22
Roman Scherer <roman.scherer@burningswell.com>
2013-09-29 19:13:16 +0200

    Added :lein-release.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/algo.monads "0.1.4"]
                  [inflections "0.8.2"]])
