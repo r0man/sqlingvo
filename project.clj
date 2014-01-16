0487617c681ded6cf3bd26bf4eb6f379614d002e
Roman Scherer <roman.scherer@burningswell.com>
2014-01-16 13:10:09 +0100

    lein-release plugin: preparing 0.5.14 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlingvo "0.5.14-SNAPSHOT"
+(defproject sqlingvo "0.5.14"
   :description "A SQL DSL in Clojure."
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
