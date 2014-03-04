ff2f93680316eedb941e011bc789ef42b27dfb97
Roman Scherer <roman.scherer@burningswell.com>
2014-03-04 20:28:47 +0100

    lein-release plugin: bumped version from 0.5.16 to 0.5.17-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlingvo "0.5.16"
+(defproject sqlingvo "0.5.17-SNAPSHOT"
   :description "A SQL DSL in Clojure."
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
