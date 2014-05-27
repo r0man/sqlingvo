2e39718fe9cb2524373b078cc3e0b53938761d43
Roman Scherer <roman.scherer@burningswell.com>
2014-05-28 00:03:34 +0200

    Add delete section

---
+++ dev/user.clj
@@ -0,0 +1,3 @@
+(ns user
+  (:refer-clojure :exclude [distinct group-by pprint])
+  (:require [sqlingvo.core :refer :all]))
--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:source-paths ["dev"]}})
