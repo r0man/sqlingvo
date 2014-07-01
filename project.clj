dac0f5ea29c9242df7985877490146b147db7386
Roman Scherer <roman.scherer@burningswell.com>
2014-07-01 21:46:17 +0200

    Deploy with Leiningen

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :lein-release {:deploy-via :clojars}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]])
