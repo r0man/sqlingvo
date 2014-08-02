c93638ac978dc54e8ae5f29e9a6906c458af54bb
Roman Scherer <roman@burningswell.com>
2014-08-02 17:34:04 +0200

    Run eastwood on travis

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:provided {:plugins [[jonase/eastwood "0.1.4"]
+                                  [lein-difftest "2.0.0"]]}}
+  :aliases {"lint" ["do" ["eastwood"]]
+            "ci" ["do" ["difftest"] ["lint"]]}
+  :eastwood {:exclude-linters [:suspicious-expression]})
