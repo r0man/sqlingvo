ce1fbef5878fc45cafdeb289dd565733d0625aff
Roman Scherer <roman@burningswell.com>
2014-11-30 23:35:34 +0100

    Add tools.namespace

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:provided {:plugins [[jonase/eastwood "0.1.4"]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]}
+             :provided {:plugins [[jonase/eastwood "0.1.4"]
                                   [lein-difftest "2.0.0"]]}}
   :aliases {"lint" ["do" ["eastwood"]]
             "ci" ["do" ["difftest"] ["lint"]]}
