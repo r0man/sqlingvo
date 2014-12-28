2957b93897e50a27543f1ed27638e3993602a09b
Roman Scherer <roman@burningswell.com>
2014-12-28 14:47:11 +0100

    Update eastwood

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]}
-             :provided {:plugins [[jonase/eastwood "0.1.4"]
+             :provided {:plugins [[jonase/eastwood "0.2.1"]
                                   [lein-difftest "2.0.0"]]}}
   :aliases {"lint" ["do" ["eastwood"]]
             "ci" ["do" ["difftest"] ["lint"]]}
