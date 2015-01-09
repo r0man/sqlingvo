327cff7fad81176476ca1735503a5081878dd459
Roman Scherer <roman@burningswell.com>
2015-01-09 11:37:06 +0100

    Remove dev dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.7"]]}
-             :provided {:plugins [[jonase/eastwood "0.2.1"]
+  :profiles {:provided {:plugins [[jonase/eastwood "0.2.1"]
                                   [lein-difftest "2.0.0"]]}}
   :aliases {"lint" ["do" ["eastwood"]]
             "ci" ["do" ["difftest"] ["lint"]]}
