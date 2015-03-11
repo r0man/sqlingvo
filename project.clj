6ff1d0199669f2a5d3f11190bfb9c94e404842fd
David James <davidcjames@gmail.com>
2015-03-11 14:02:10 -0400

    exclude `update` for Clojure 1.7

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]]
   :profiles {:provided {:plugins [[jonase/eastwood "0.2.1"]
                                   [lein-difftest "2.0.0"]]}}
   :aliases {"lint" ["do" ["eastwood"]]
