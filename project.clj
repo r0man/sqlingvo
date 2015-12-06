9604632e19ec03da9c470401fb5afee38f823af5
Roman Scherer <roman@burningswell.com>
2015-12-06 17:46:59 +0100

    Update eastwood

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:provided {:plugins [[jonase/eastwood "0.2.1"]
+  :profiles {:provided {:plugins [[jonase/eastwood "0.2.2"]
                                   [lein-difftest "2.0.0"]]}}
   :aliases {"lint" ["do" ["eastwood"]]
             "ci" ["do" ["difftest"] ["lint"]]}
