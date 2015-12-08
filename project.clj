b71d36543723f3de7e5195e85cdad0457a1c1da6
Roman Scherer <roman@burningswell.com>
2015-12-08 15:05:42 +0100

    Version 0.8.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlingvo "0.8.1"
+(defproject sqlingvo "0.8.2-SNAPSHOT"
   :description "A SQL DSL in Clojure."
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
