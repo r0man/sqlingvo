a2a1d3d896b3b49131621b97ca8ca7e56147e05f
Roman Scherer <roman@burningswell.com>
2015-12-08 13:26:40 +0100

    Revert "Add database drivers"
    This reverts commit 9d84dccdc874167f5fb89eea390f9d3d13f96533.

--- project.clj
+++ project.clj
@@ -4,12 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.postgresql/postgresql "9.4-1206-jdbc42"]]
-  :profiles {:dev {:dependencies [[funcool/clojure.jdbc "0.6.1"]
-                                  [org.clojure/java.jdbc "0.4.2"]]
-                   :plugins [[jonase/eastwood "0.2.2"]
-                             [lein-difftest "2.0.0"]]}}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:provided {:plugins [[jonase/eastwood "0.2.2"]
+                                  [lein-difftest "2.0.0"]]}}
   :aliases {"lint" ["do" ["eastwood"]]
             "ci" ["do" ["difftest"] ["lint"]]}
   :eastwood {:exclude-linters [:suspicious-expression]})
