4692d858aa7bd7f0ea933a53e8a43da4b9eec7f4
Roman Scherer <roman@burningswell.com>
2016-02-01 16:02:31 +0100

    Use clojure 1.8

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:provided {:plugins [[jonase/eastwood "0.2.2"]
                                   [lein-difftest "2.0.0"]]}}
   :aliases {"lint" ["do" ["eastwood"]]
