30cab02625af511e13f6cb0d4f8b8a50aeb2b43b
r0man <roman@burningswell.com>
2017-05-07 15:57:15 +0200

    Version 0.9.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlingvo "0.9.6"
+(defproject sqlingvo "0.9.7-SNAPSHOT"
   :description "A Clojure DSL to create SQL statements"
   :url "http://github.com/r0man/sqlingvo"
   :license {:name "Eclipse Public License"
