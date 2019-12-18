4b2bcb68eaf15f7621a199b2922da919f4bf5460
r0man <roman@burningswell.com>
2019-12-18 17:42:44 -0300

    Update dependencies (#120)

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :min-lein-version "2.5.2"
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[noencore "0.3.6"]
-                 [org.clojure/clojure "1.10.0"]]
-  :plugins [[jonase/eastwood "0.3.5"]
+                 [org.clojure/clojure "1.10.1"]]
+  :plugins [[jonase/eastwood "0.3.6"]
             [lein-cljsbuild "1.1.7"]
             [lein-difftest "2.0.0"]
             [lein-doo "0.1.11"]]
@@ -15,13 +15,13 @@
              :exclude-linters [:local-shadows-var]}
   :profiles
   {:dev
-   {:dependencies [[org.clojure/test.check "0.9.0"]]}
+   {:dependencies [[org.clojure/test.check "0.10.0"]]}
    :provided
-   {:dependencies [[org.clojure/clojurescript "1.10.520"]]}
+   {:dependencies [[org.clojure/clojurescript "1.10.597"]]}
    :repl
    {:dependencies [[com.cemerick/piggieback "0.2.2"]
                    [reloaded.repl "0.2.4"]]
-    :plugins [[figwheel-sidecar "0.5.18"]]
+    :plugins [[figwheel-sidecar "0.5.19"]]
     :init-ns user
     :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
   :aliases
